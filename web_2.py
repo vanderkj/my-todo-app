bc64f77096be212654758ce65d9a9093f85b66ad
vanderkj <69473906+vanderkj@users.noreply.github.com>
2025-01-02 16:14:23 -0700

    New app with a unique key top avoid crash upon duplicate item entry

---
+++ web_2.py
@@ -0,0 +1,27 @@
+import streamlit as st
+import functions_2
+import time
+
+todos = functions_2.get_todos()
+blank = ''
+
+def add_todo():
+    current_time = time.strftime("%I%M%S")
+    todo = current_time + st.session_state["new_todo"] + "\n"
+    todos.append(todo)
+    functions_2.save_todos(todos)
+    st.session_state["new_todo"] = blank
+
+st.title("My Todo App")
+st.subheader("This is my Todo app")
+
+for index,todo in enumerate(todos):
+    checkbox = st.checkbox(todo[6:], key=todo)
+    if checkbox:
+        todos.pop(index)
+        functions_2.save_todos(todos)
+        del st.session_state[todo]
+        st.rerun()
+
+st.text_input(" ",placeholder="Add a new todo ...",
+              on_change=add_todo, key="new_todo")
